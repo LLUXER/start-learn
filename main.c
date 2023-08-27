57297bf7ac729f320afa9e344f68ff1d204631ff
arrtist <vadimkuku283@gmail.com>
2023-08-27 18:01:20 +0300

    first commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{   
+    char hello[] = "Hello World!";
+
+    printf("Hello world ");
+    printf("%s",hello);
+    printf("\n");
+
+    printf("1\t2\t");
+
+    return 0;
+}
